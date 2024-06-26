70d6a82ba8572b699fa70e5f7ecb1c1a71163360
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-06-26 23:52:51 +0800

    Added initial inbox page

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Conversation = () => {
+  return (
+    <div className="px-6 py-4 border border-gray-300 rounded-xl cursor-pointer">
+      <p className="mb-6 text-xl">person name</p>
+      <p className="text-sharebnb-dark">Go to conversation</p>
+    </div>
+  );
+};
+
+export default Conversation;
