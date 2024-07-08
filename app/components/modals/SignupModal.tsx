2856a9528cf2c658e2544f32aa5f24ce4e3a997a
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-07-09 00:58:48 +0800

    handle call for signup user

--- app/components/modals/SignupModal.tsx
+++ app/components/modals/SignupModal.tsx
@@ -1,15 +1,52 @@
 "use client";
 
 import useSignupModal from "@/app/hooks/useSignupModal";
-import React from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
 import CustomButton from "../form/CustomButton";
+import { useRouter } from "next/navigation";
+import apiService from "@/app/services/apiService";
+import { error } from "console";
 
 const SignupModal = () => {
+  const router = useRouter();
   const signupModal = useSignupModal();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const handleSignUpSubmit = async () => {
+    const formData = {
+      email: email,
+      password1: password,
+      password2: confirmPassword,
+    };
+
+    const response = await apiService.post(
+      "/api/auth/register/",
+      JSON.stringify(formData)
+    );
+
+    if (response.access) {
+      // handleLogin()
+      signupModal.close();
+      router.push("/");
+      setErrors([]);
+    } else {
+      const responseErrors: string[] = [];
+      for (const [key, value] of Object.entries(response)) {
+        responseErrors.push(`${key}: ${value}`);
+      }
+
+      setErrors(responseErrors);
+    }
+  };
+
   const content = (
-    <form action="" className="space-y-4">
+    <form className="space-y-4">
       <input
+        onChange={(e) => setEmail(e.target.value)}
         type="email"
         name="email"
         id="email"
@@ -17,6 +54,7 @@ const SignupModal = () => {
         className="w-full h-[54px] border border-gray-300 rounded-xl px-4"
       />
       <input
+        onChange={(e) => setPassword(e.target.value)}
         type="password"
         name="password"
         id="password"
@@ -24,16 +62,23 @@ const SignupModal = () => {
         className="w-full h-[54px] border border-gray-300 rounded-xl px-4"
       />
       <input
-        type="repeatpassword"
+        onChange={(e) => setConfirmPassword(e.target.value)}
+        type="password"
         name="repeatpassword"
         id="repeatpassword"
         placeholder="Repeat Password"
         className="w-full h-[54px] border border-gray-300 rounded-xl px-4"
       />
-      <div className="p-4 bg-sharebnb text-white rounded-xl opacity-80">
-        Error message
-      </div>
-      <CustomButton label="Submit" onClick={() => console.log("Signup")} />
+      {errors.map((error, index) => (
+        <div
+          key={index}
+          className="p-4 bg-sharebnb text-white rounded-xl opacity-80"
+        >
+          {error}
+        </div>
+      ))}
+
+      <CustomButton label="Submit" onClick={handleSignUpSubmit} />
     </form>
   );
   return (
