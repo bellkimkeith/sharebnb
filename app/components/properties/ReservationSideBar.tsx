073ce4db9404bf704145f4e98da1888a59f461b6
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-08-08 11:15:36 +0800

    added date picker and submit booking

--- app/components/properties/ReservationSideBar.tsx
+++ app/components/properties/ReservationSideBar.tsx
@@ -1,20 +1,151 @@
-import React from "react";
+"use client";
+import React, { ReactNode, useEffect, useState } from "react";
 import { PropertyType } from "./PropertiesList";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { Range } from "react-date-range";
+import { differenceInDays, format } from "date-fns";
+import DatePicker from "../form/DatePicker";
+import apiService from "@/app/services/apiService";
 
-const ReservationSideBar = ({ property }: { property: PropertyType }) => {
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(new Date().setDate(new Date().getDate() + 1)),
+  key: "selection",
+};
+
+type ReservationType = {
+  fee: string;
+  nights: string;
+  totalPrice: string;
+  dateRange: Range;
+  minDate: Date;
+  guests: string;
+};
+
+const ReservationSideBar = ({
+  property,
+  userId,
+}: {
+  property: PropertyType;
+  userId: string | undefined;
+}) => {
   const createGuestsOptions = () => {
     const options = [];
     for (let index = 1; index <= parseInt(property.guests); index++) {
-      options.push(<option value={index}>{index}</option>);
+      options.push(
+        <option key={index} value={index}>
+          {index}
+        </option>
+      );
     }
     return options;
   };
   const guestsOptions = createGuestsOptions();
+  const loginModal = useLoginModal();
+  const [reservationData, setReservationData] = useState<ReservationType>({
+    fee: "0",
+    nights: "1",
+    totalPrice: "0",
+    dateRange: initialDateRange,
+    minDate: new Date(),
+    guests: "1",
+  });
+
+  const selectDateRange = (selection: any) => {
+    const newStartDate = new Date(selection.startDate);
+    const newEndDate = new Date(selection.endDate);
+
+    if (newEndDate <= newStartDate) {
+      newEndDate.setDate(newStartDate.getDate() + 1);
+    }
+
+    setReservationData((prev) => ({
+      ...prev,
+      dateRange: {
+        ...prev.dateRange,
+        startDate: newStartDate,
+        endDate: newEndDate,
+      },
+    }));
+  };
+
+  const submitBooking = async () => {
+    if (userId) {
+      if (
+        reservationData.dateRange.startDate &&
+        reservationData.dateRange.endDate
+      ) {
+        const formData = new FormData();
+        formData.append("guests", reservationData.guests);
+        formData.append(
+          "start_date",
+          format(reservationData.dateRange.startDate, "yyyy-MM-dd")
+        );
+        formData.append(
+          "end_date",
+          format(reservationData.dateRange.endDate, "yyyy-MM-dd")
+        );
+        formData.append("number_of_nights", reservationData.nights);
+        formData.append("total_price", reservationData.totalPrice);
+
+        const response = await apiService.post(
+          `/api/properties/${property.id}/book/`,
+          formData
+        );
+
+        if (response.success) {
+          console.log("Booking successful");
+        } else {
+          console.log("Something went wrong in booking");
+        }
+      }
+    } else {
+      loginModal.open();
+    }
+  };
+
+  useEffect(() => {
+    if (
+      reservationData.dateRange.startDate &&
+      reservationData.dateRange.endDate
+    ) {
+      const dayCount = differenceInDays(
+        reservationData.dateRange.endDate,
+        reservationData.dateRange.startDate
+      );
+
+      if (dayCount && property.price_per_night) {
+        const reservationFee = dayCount * property.price_per_night * 0.05;
+        setReservationData((prev) => ({
+          ...prev,
+          fee: reservationFee.toString(),
+          totalPrice: (
+            property.price_per_night * dayCount +
+            reservationFee
+          ).toString(),
+          nights: dayCount.toString(),
+        }));
+      } else {
+        const reservationFee = property.price_per_night * 0.05;
+        setReservationData((prev: ReservationType) => ({
+          ...prev,
+          fee: reservationFee.toString(),
+          totalPrice: (property.price_per_night + reservationFee).toString(),
+          nights: "1",
+        }));
+      }
+    }
+  }, [reservationData.dateRange, property.price_per_night]);
+
   return (
-    <aside className="col-span-2 rounded-xl border border-gray-300 shadow-xl p-6 mt-6">
+    <aside className="col-span-2 rounded-xl border border-gray-300 shadow-xl p-6 mt-6 mx-auto">
       <h2 className="mb-5 text-xl">
         <strong>₱{property.price_per_night}</strong> per night
       </h2>
+      <DatePicker
+        value={reservationData.dateRange}
+        onChange={(value) => selectDateRange(value.selection)}
+      />
       <div className="mb-6 p-3 border border-gray-400 rounded-xl">
         <label className="mb-2" htmlFor="guests">
           Guests
@@ -23,29 +154,40 @@ const ReservationSideBar = ({ property }: { property: PropertyType }) => {
           className="w-full text-sm focus:outline-none"
           name="guests"
           id="guests"
+          value={reservationData.guests}
+          onChange={(e) =>
+            setReservationData({ ...reservationData, guests: e.target.value })
+          }
         >
-          {/* <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option> */}
-          {guestsOptions.map((option: any) => option)}
+          {guestsOptions.map((option: ReactNode) => option)}
         </select>
       </div>
-      <div className="w-full mb-6 py-4 text-center text-white bg-sharebnb hover:bg-sharebnb-dark rounded-xl cursor-pointer">
+      <div
+        onClick={submitBooking}
+        className="w-full mb-6 py-4 text-center text-white bg-sharebnb hover:bg-sharebnb-dark rounded-xl cursor-pointer"
+      >
         Book
       </div>
       <div className="flex mb-4 justify-between align-middle">
-        <span>₱{property.price_per_night} x 3 nights</span>
-        <span>₱30,000</span>
+        <span>
+          ₱{property.price_per_night} x {reservationData.nights} night(s)
+        </span>
+        <span>
+          ₱{property.price_per_night * parseInt(reservationData.nights)}
+        </span>
       </div>
       <div className="flex mb-4 justify-between align-middle">
         <span>ShareBnb fee</span>
-        <span>₱500</span>
+        <span>₱{reservationData.fee}</span>
       </div>
       <hr />
       <div className="flex mt-4 justify-between align-middle">
         <span>Total</span>
-        <span>₱30,500</span>
+        <span>
+          ₱
+          {property.price_per_night * parseInt(reservationData.nights) +
+            parseInt(reservationData.fee)}
+        </span>
       </div>
     </aside>
   );
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -3,10 +3,11 @@ import ReservationSideBar from "@/app/components/properties/ReservationSideBar";
 import Image from "next/image";
 import React from "react";
 import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/actions";
 
 const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
   const property = await apiService.get(`/api/properties/${params.id}`);
-  console.log("proeprass", property);
+  const userId = await getUserId();
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
       <div className="w-full h-[64vh] overflow-hidden rounded-xl relative mb-4">
@@ -19,7 +20,7 @@ const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
         <PropertyDetails property={property} />
-        <ReservationSideBar property={property} />
+        <ReservationSideBar property={property} userId={userId} />
       </div>
     </main>
   );
